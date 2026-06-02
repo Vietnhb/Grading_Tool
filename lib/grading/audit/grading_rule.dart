6940412968ee86c7af04655869a39619b57a3e04
phucplhse183189 <phucplhse183189@fpt.edu.vn>
2026-06-02 17:35:21 +0700

    cleanup: remove unused AIAuditor and audit rules (dead code not integrated into main flow)

--- lib/grading/audit/grading_rule.dart
+++
@@ -1,7 +0,0 @@
-import '../grading_models.dart';
-import 'ai_grading_response.dart';
-import 'audit_result.dart';
-
-abstract class GradingRule {
-  AuditResult evaluate(AIGradingResponse response, GradingGuide guide);
-}
